9fe50476cc4b738756fe72dad3d278a7b7f57a40
matsuyamakaito <eringi.matsuyamakaito@gmail.com>
2022-03-08 01:51:55 +0900

    プロフィール画面の作成

---
+++ resources/views/profiles/profile.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.profile_header')
+@section('content')
+<div class="container profile-container">
+    <h2 class="profile-title">プロフィール</h2>
+    <div class="profile-wrapper">
+        <dl>
+            <dt>ユーザーネーム</dt>
+            <dd>{{$user->name}}</dd>
+        </dl>
+        <dl>
+            <dt>在籍している大学</dt>
+            <dd>{{$user->university->name}}</dd>
+        </dl>
+        <dl>
+            <dt>興味のある学問分野</dt>
+            <dd>
+                @foreach($user->categories as $category)
+                {{$category->name}}
+                @endforeach
+            </dd>
+        </dl> 
+    </div>
+    <a href="/profile/edit" type="button" class="btn btn-success edit-btn">編集</a>
+</div>
+<div class="show-textbooks">
+    <a>購入した教科書</a>
+    <a>出品した教科書</a>
+</div>
+@endsection 
