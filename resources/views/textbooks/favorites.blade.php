50ebcff46632e6da8e1900d98537125f18e6a7aa
matsuyamakaito <eringi.matsuyamakaito@gmail.com>
2022-03-11 18:09:43 +0900

    ページタイトル・項目を英語表記に変更

--- resources/views/textbooks/favorites.blade.php
+++ resources/views/textbooks/favorites.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.textbook_index_header')
 @section('content')
 <div class="container index-container">
-    <h2 class="index-title">気になる教科書</h2>
+    <h2 class="index-title">Favorite Textbooks</h2>
     <ul>
         @foreach($user->favoriteTextbooks as $favoriteTextbook)
         <li>
             <div class="index-container">
               <img src="{{ asset('storage/'.$favoriteTextbook->image) }}" width="200px" height="200px">
               <div class="text-wrapper">
-                  <p>タイトル：{{ $favoriteTextbook->name }}</p>
-                  <p>カテゴリー：{{ $favoriteTextbook->category->name}}</p>
+                  <p>Title：{{ $favoriteTextbook->name }}</p>
+                  <p>Category：{{ $favoriteTextbook->category->name}}</p>
               </div>
               <a href="/textbook/{{ $favoriteTextbook->id }}" class="btn btn-success index-btn">詳細をみる</a>
             </div>
