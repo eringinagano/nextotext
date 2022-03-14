f91152d64dc884d57d3979b93bc1c8faa36e53ab
matsuyamakaito <eringi.matsuyamakaito@gmail.com>
2022-03-14 15:21:27 +0900

    usersテーブルにimageカラム追加・プロフィール編集画面のレイアウト変更

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'university_id',
+        'name', 'image', 'email', 'password', 'university_id',
     ];
 
     /**
