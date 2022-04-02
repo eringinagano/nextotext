0ef81bb735518a546cf7d165b0d696c30b402be9
matsuyamakaito <eringi.matsuyamakaito@gmail.com>
2022-04-02 19:30:06 +0900

    チャット内容保存処理・表示処理の作成

--- app/Message.php
+++ app/Message.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     //
+    protected $fillable = [
+        'group_id', 'message',
+    ];
+    
     public function Group() {
         return $this->belongsTo('App\Group');
     }
