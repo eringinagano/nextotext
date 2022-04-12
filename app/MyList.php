94f43062236509a3e663fc1d22adffbe1afdf842
matsuyamakaito <eringi.matsuyamakaito@gmail.com>
2022-04-12 17:08:44 +0900

    マイリスト追加機能の修正

--- app/MyList.php
+++ app/MyList.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class MyList extends Model
 {
     //
+    protected $fillable = [
+        'textbook_name', 'author_name'
+    ];
+    
     public function users() {
         return $this->belongsTo('App\User');
     }
