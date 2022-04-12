f478b1dfc2dc98043c498f2842a9f3a6519a3503
matsuyamakaito <eringi.matsuyamakaito@gmail.com>
2022-04-12 18:09:34 +0900

    マイリスト追加機能の修正

--- resources/views/mylists/mylist.blade.php
+++ resources/views/mylists/mylist.blade.php
@@ -3,13 +3,15 @@
 <div class="container mylist-container">
     <h2 class="mylist-title">My List</h2>
     <div class="mylist-wrapper">
-      <ol class="mylist">
-      @foreach( $user->mylists as $mylist)  
-        <li>タイトル：{{ $mylist->textbook_name }}<br>
-            著者：{{ $mylist->author_name }}
-        </li>
-      @endforeach  
-      </ol>
+      <ul class="list-group">
+        @foreach( $user->mylists as $mylist)  
+          <li class="list-group-item">
+              <p>タイトル：{{ $mylist->textbook_name }}</p>
+              <p>著者：{{ $mylist->author_name }}</p>
+              <a href="" class="btn btn-outline-danger delete-btn">削除</a>
+          </li>
+        @endforeach
+      </ul>
     </div>
     <div class="btn-wrapper">
         <a href="/mylist/register" class="btn btn-outline-success mylist-btng">マイリスト追加</a>
